6737580b9c026c6d953bcacb70c5e31256e77365
Yojana Subedi <yosulalala@gmail.com>
2024-02-06 21:07:58 +0545

    feat: added product list view model

---
+++ Service/Category/Query/GetProductList/ProductListViewModel.cs
@@ -0,0 +1,16 @@
+public class ProductListViewModel
+{
+
+   public string? Id { get; set; }
+
+   public string? Name { get; set; }
+   public int Quantity { get; set; }
+   public decimal Price { get; set; }
+
+   public string? Category { get; set; }
+
+
+   public DateTime CreatedAt { get; set; }
+
+
+}
