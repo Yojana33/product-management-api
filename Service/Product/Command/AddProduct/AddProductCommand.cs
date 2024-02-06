e0c3a5c75b28605b47d3e20ff4cbc767aacc907e
Yojana Subedi <yosulalala@gmail.com>
2024-02-06 21:08:53 +0545

    feat: added add product command

---
+++ Service/Product/Command/AddProduct/AddProductCommand.cs
@@ -0,0 +1,12 @@
+public class AddProductCommand
+{
+    public string? ProductName{get; set;}
+
+    public int ProductQuantity {get; set;}
+
+    public decimal ProductPrice {get; set;}
+
+     public string ProductDescription{get; set;}=" ";
+
+   public string? CategoryId{get; set;}
+}
