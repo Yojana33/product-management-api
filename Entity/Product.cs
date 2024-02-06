c78c65525835f2b988309a5ef9ada65c58c450d5
Yojana Subedi <yosulalala@gmail.com>
2024-02-06 20:50:54 +0545

    feat: added product class

---
+++ Entity/Product.cs
@@ -0,0 +1,19 @@
+public class Product : Baseclass{
+
+   
+
+   public string? Name{ get; set;}
+
+   public int Quantity {get; set;}
+
+   public decimal Price {get; set;}
+
+   public string Description{get; set;}=" ";
+
+   public string? CategoryId{get; set;}
+   public string? Category { get; set; }
+
+
+
+
+}
